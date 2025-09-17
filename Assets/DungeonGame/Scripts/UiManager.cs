3f7ffbfc12a54783b751be4d167e2c9a348539ba
LucasCoimbraCarty <lucas.cartybr@gmail.com>
2025-09-17 09:15:00 -0300

    Menu-Pause

---
+++ Assets/DungeonGame/Scripts/UiManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("Main");
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
